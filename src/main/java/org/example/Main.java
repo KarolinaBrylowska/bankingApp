665a9bc596f0b15522956425fd9d81963bbbb74b
karaolinabrylowska <kar.bronk@gmail.com>
2022-10-03 18:42:13 +0200

    Added new class account.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to our Bank!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please, enter your name: ");
+        String name=sc.next();
+        System.out.println("Please, enter your surname: ");
+        String surname=sc.next();
+
+        Account user=new Account(name, surname);
+    }
+}
